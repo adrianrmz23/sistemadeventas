8bb48a4cedcbe1e1eb239de9aa9aea215944094d
ADRIAN <adrian23081996@gmail.com>
2024-11-05 19:15:30 -0600

    commit

---
+++ app/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Variables Globales */
+define('SERVER','localhost');
+define('USER','root');
+define('PASSWORD','wesito15');
+define('BD','sistemadeventas');
+
+$servidor = "mysql:dbname=".BD.";host=".SERVER;
+
+try{
+    $pdo = new PDO($servidor, USER, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    //echo "La conexión a la base de datos fue exitosa";
+}catch (PDOException $e){
+    //print_r($e);
+    echo "Error al conectar a la base de datos.";
+}
